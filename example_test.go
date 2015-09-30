fbef2d4922996d34466d668eb379d1deee63e281
Liu Ming <extrafliu@gmail.com>
2015-09-30 11:44:14 +0800

    add some example

---
+++ example_test.go
@@ -0,0 +1,28 @@
+package xls
+
+import (
+	"fmt"
+)
+
+func ExampleOpen() {
+	if xlFile, err := Open("Table.xls", "utf-8"); err == nil {
+		fmt.Println(xlFile.Author)
+	}
+}
+
+//Output: read the content of first two cols in each row
+func ExampleWorkBook_GetSheet() {
+	if xlFile, err := Open("Table.xls", "utf-8"); err == nil {
+		if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
+			fmt.Print("Total Lines ", sheet1.MaxRow, sheet1.Name)
+			col1 := sheet1.Rows[0].Cols[0]
+			col2 := sheet1.Rows[0].Cols[0]
+			for i := 0; i <= (int(sheet1.MaxRow)); i++ {
+				row1 := sheet1.Rows[uint16(i)]
+				col1 = row1.Cols[0]
+				col2 = row1.Cols[1]
+				fmt.Print("\n", col1.String(xlFile), ",", col2.String(xlFile))
+			}
+		}
+	}
+}
--- xls_test.go
+++ xls_test.go
@@ -44,24 +44,3 @@ func TestMaxRow(t *testing.T) {
 		}
 	}
 }
-
-//read the content of first two cols in each row
-func ExampleReadXls(t *testing.T) {
-	xlFile, err := Open("Table.xls", "utf-8")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failure: %v\n", err)
-		t.Error(err)
-	}
-
-	if sheet1 := xlFile.GetSheet(0); sheet1 != nil {
-		fmt.Print("Total Lines ", sheet1.MaxRow, sheet1.Name)
-		col1 := sheet1.Rows[0].Cols[0]
-		col2 := sheet1.Rows[0].Cols[0]
-		for i := 0; i <= (int(sheet1.MaxRow)); i++ {
-			row1 := sheet1.Rows[uint16(i)]
-			col1 = row1.Cols[0]
-			col2 = row1.Cols[1]
-			fmt.Print("\n", col1.String(xlFile), ",", col2.String(xlFile))
-		}
-	}
-}
