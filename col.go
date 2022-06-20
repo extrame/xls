171d55861f21230b4bdd6a5cf64eb2f819d97c0e
elbert-widjaja <elbert.widjaja@bytedance.com>
2022-06-20 14:16:33 +0800

    fix datetime xls for yyyy/mm/dd hh/mm/ss

--- col.go
+++ col.go
@@ -11,7 +11,7 @@ import (
 	yymmdd "github.com/extrame/goyymmdd"
 )
 
-const DDMMYYYYhhmmss = "2006-01-02 15:04:05"
+const DDMMYYYYhhmmss = "2006\\01\\02 15:04:05"
 
 //content type
 type contentHandler interface {
